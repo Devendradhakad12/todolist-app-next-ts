85cbb3b21624d5f9053d36340e25fbc6e5253eb1
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-10-28 09:33:20 +0530

    models created

---
+++ models/task.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { models } from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  checked:{
+    type:Boolean,
+    default:false
+  }
+});
+
+export const Task = models.Task ||  mongoose.model("Task", TaskSchema);
---
+++ models/user.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { models } from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+export const User = models.User || mongoose.model("User", UserSchema);
