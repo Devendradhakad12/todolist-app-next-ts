1fe4266d64ec0f4c646a15d84e5e537651f3dfeb
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-10-30 09:49:28 +0530

    missing dependencies bug fix - ./app/(route)/edit/[taskid]/page.tsx

--- app/(route)/edit/[taskid]/page.tsx
+++ app/(route)/edit/[taskid]/page.tsx
@@ -13,92 +13,93 @@ import toast from 'react-hot-toast';
 import { UserDataProps } from '../../tasks/all/page';
 import { fetchTodo } from '@/redux/action/todoActions';
 
-const EditTask = ({params}:{params:{taskid:string}}) => {
-    const {data} = useSession()
+const EditTask = ({ params }: { params: { taskid: string } }) => {
+    const { data, status } = useSession()
     const router = useRouter()
-    if(!data?.user) return  router.push("/")
-    const [userData, setUserData] = useState<UserDataProps>() 
-    const {tasks} = useAppSelector((state)=>state.todo)
+    if (!data?.user) return router.push("/")
+    const [userData, setUserData] = useState<UserDataProps>()
+    const { tasks } = useAppSelector((state) => state.todo)
     const dispatch = useAppDispatch()
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [loading,setLoading] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-    const currentTask  = tasks.filter((t) => {
-        return t._id === params.taskid;
-      });
-
-
-      useEffect(() => {
+    const [currentTask, setCurrentTask] = useState<InitialStateProps["tasks"]>([])
+   
+    console.log(currentTask)
+    useEffect(() => {
         setUserData(data?.user)
-        if(currentTask[0]?.title !== undefined && currentTask[0]?.title !== null)
-        setTitle(currentTask[0]?.title);
-       setDescription(currentTask[0]?.description!);
-      }, [tasks]);
-     
-   async function reFetch(){
+        setCurrentTask(tasks.filter((t) => {
+            return t._id === params.taskid;
+        }))
+        if (currentTask[0]?.title !== undefined && currentTask[0]?.title !== null)
+            setTitle(currentTask[0]?.title);
+        setDescription(currentTask[0]?.description!);
+    }, [data, status, router, tasks, dispatch, userData?.id]);
+
+    async function reFetch() {
         setUserData(data?.user)
         if (userData?.id) {
-        const bool = await  fetchTodo({ userid: userData?.id, dispatch }) 
-          if(bool) return true
+            const bool = await fetchTodo({ userid: userData?.id, dispatch })
+            if (bool) return true
         }
     }
 
 
-      const hnadleSubmit =async (e:any) =>{
+    const hnadleSubmit = async (e: any) => {
         e.preventDefault();
         setLoading(true)
         try {
             await axios.patch(`/api/task/${currentTask[0]._id}/${userData?.id}`, {
-                title,description
+                title, description
             });
 
             toast.success("Task updated")
             const bool = await reFetch()
-            if(bool) return router.push("/tasks/all")
-            
+            if (bool) return router.push("/tasks/all")
+
         } catch (error) {
             toast.error("Something went Wrong")
             console.log(error)
-        }finally{
+        } finally {
             setLoading(false)
         }
 
-      }
+    }
 
-  return (
-    <>
+    return (
+        <>
 
-    <div className="mainDiv">
-        <div className="loginDiv">
-            <h1 className="loginH1">Add Your Task</h1>
-            <form className="loginForm py-10" onSubmit={hnadleSubmit}>
-                <Input
-                    type="text"
-                    placeholder="Task Title"
-                    className="formInput"
-                    required
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
-                    value={title}
-                />
-                <Textarea
-                    rows={1}
-                    placeholder="Task Description"
-                    className="formInput"
-                    required
-                    onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setDescription(e.target.value)}
-                    value={description}
-                />
+            <div className="mainDiv">
+                <div className="loginDiv">
+                    <h1 className="loginH1">Add Your Task</h1>
+                    <form className="loginForm py-10" onSubmit={hnadleSubmit}>
+                        <Input
+                            type="text"
+                            placeholder="Task Title"
+                            className="formInput"
+                            required
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+                            value={title}
+                        />
+                        <Textarea
+                            rows={1}
+                            placeholder="Task Description"
+                            className="formInput"
+                            required
+                            onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setDescription(e.target.value)}
+                            value={description}
+                        />
 
-                <button type="submit" className="lsBtn">
-                    {loading ? "Submit..." : "Submit"}
-                </button>
-            </form>
-        </div>
-    </div>
+                        <button type="submit" className="lsBtn">
+                            {loading ? "Submit..." : "Submit"}
+                        </button>
+                    </form>
+                </div>
+            </div>
 
-</>
-  )
+        </>
+    )
 }
 
 export default EditTask
